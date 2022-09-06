50e6118ddfc8415afb999cb503eb2dcd35af6640
Scout Cox <coxscout@gmail.com>
2022-09-06 14:38:35 -0400

    commit tree b11b24acfcaafb363bfb8d27976390e594245651

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const element = document.getElementById('main');
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent= "Scout is the champion"
